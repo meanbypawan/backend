008027af157ee533a2ec36401d7edf16f41b77a9
meanstack <meanstack4@gmail.com>
2022-03-28 19:19:34 +0530

    sign-in api

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,16 @@ const User = require('../model/user');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post("/signin",(request,response)=>{
+  User.find({email: request.body.email,password: request.body.password})
+  .then(result=>{
+    //console.log(result);
+    response.json(result);
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+});
 router.post("/signup",(request,response)=>{
    User.create({
     username: request.body.username,
